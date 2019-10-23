66a6f9d09b1e41a4fbed8cbfa513bc01eaae0041
tms <tmerrittsmith@gmail.com>
2019-10-23 20:34:25 +0100

    first commit

---
+++ probability_noise.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+def probability_noise(array, n):
+    max_prob = np.random.random() * (1 - 1 / n) + 1 / n
+    rem_probs = np.random.dirichlet(np.ones(n-1)) * (1 - max_prob)
+    prob_array = np.insert(rem_probs, np.argmax(array), max_prob)
+    return prob_array
+    
+    
+def probability_noise_one_hot(array):
+    ## takes as argument a onehot encoded array
+    ## returns a probability noised array, where the max probability of each row
+    ## has the same index as the original 1- in the row
+    
+    n = array.shape[1]
+    rows = array.shape[0]
+    max_idx = np.argmax(array, axis=1)
+    max_probs = np.random.random(rows) * (1 - 1 / n) + 1 / n
+    remaining_probs = np.random.dirichlet(np.ones(n-1), size=rows) * (1 - max_probs)[:,None]
+    noised_probs = []
+    for rem, maxprob, i in zip(remaining_probs, max_probs, max_idx):
+        noised_probs.append(np.insert(rem, i, maxprob))
+    noised_probs = np.array(noised_probs)
+    return noised_probs
+    
+    
+    
